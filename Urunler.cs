394f7a3e6c6d6b3048390f926ada555edc173076
gulenzek <73951604+gulenzek@users.noreply.github.com>
2020-11-30 09:39:34 +0300

    Update Urunler.cs

--- Urunler.cs
+++ Urunler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace AlisverisSepeti
 {
-    class Urunler
+    class Urunler : IEnumerable
     {
+        Urun urun = new Urun();
+
         public Urun UrunGetir(int id)
         {
-            Urun urun = new Urun();
-
             if (true)
             {
                 urun._id = 1;
@@ -28,5 +29,86 @@ namespace AlisverisSepeti
         {
             return new List<Urun>();
         }
+
+        IEnumerator GetEnumerator()
+        {
+            return (IEnumerator<Urun>)GetEnumerator();
+        }
+
+        private bool isDisposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            // IDisposable..
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    if (urun != null)
+                        urun.Dispose();
+                }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class UrunlerEnum: IEnumerator<Urun>
+    {
+        public Urun[] _urun;
+        int pozisyon = -1;
+
+        public UrunlerEnum(Urun[] urunListesi)
+        {
+            _urun = urunListesi;
+        }
+        public bool MoveNext()
+        {
+            pozisyon++;
+            return (pozisyon<_urun.Length);
+        }
+        public void Reset()
+        {
+            pozisyon = -1;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        /*
+        object IEnumerator.MevcutUrun
+        {
+            get
+            {
+                return MevcutUrun;
+            }
+        }
+        */
+        public Urun MevcutUrun
+        {
+            get
+            {
+                try
+                {
+                    return _urun[pozisyon];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+        }
+
+        public Urun Current => throw new NotImplementedException();
+
+        object System.Collections.IEnumerator.Current => throw new NotImplementedException();
     }
 }
